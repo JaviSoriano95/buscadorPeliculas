a5220835196cef2abb76c937e15e329d4d2e27e3
=Javi <javisoriano3@gmail.com>
2024-05-26 22:13:58 +0200

    mejoras app

--- src/app/pages/mainPage/mainPage.component.ts
+++ src/app/pages/mainPage/mainPage.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { Movie, Search } from '../../interfaces/movie';
-import { FormControl } from '@angular/forms';
+import { FormControl, FormGroup } from '@angular/forms';
 import { SearchService } from '../../services/search.service';
 
 @Component({
@@ -9,29 +9,34 @@ import { SearchService } from '../../services/search.service';
   styleUrls: ['./mainPage.component.css'],
 })
 export class MainPageComponent {
-  public searchInput = new FormControl('');
+  public searchInput = new FormControl<string>('');
+  public checkInput = new FormControl<Boolean>(false);
 
   public movies: Search[] = [];
-  public messageError: string =''
-  public lastParams: string='';
+  public moviesSort: Search[] = [];
+  public moviesKeep: Search[] = [];
+  public messageError: string = '';
+  public lastParams: string = '';
 
   constructor(private searchService: SearchService) {}
 
   buscar() {
 
-    if(!this.validar()){
+    if (!this.validar()) {
       return;
     }
 
-    this.messageError= '';
+    this.messageError = '';
 
+    this.searchService
+      .getFilms(this.searchInput.value!)
+      .subscribe((result: Movie) => {
+        this.movies = result.Search.map((item: Search) =>
+          this.transformMovie(item)
+        );
+      });
 
-
-    this.searchService.getFilms(this.searchInput.value!).subscribe((result: Movie) => {
-      this.movies = result.Search.map((item: Search) =>
-        this.transformMovie(item)
-      );
-    });
+      this.checkInput.setValue(false);
   }
   transformMovie(movie: any): Search {
     return {
@@ -43,26 +48,38 @@ export class MainPageComponent {
     };
   }
 
-
-  validar(){
-    if(!this.searchInput.value || this.searchInput.value.length < 3) {
+  validar() {
+    if (!this.searchInput.value || this.searchInput.value.length < 3) {
       this.messageError = 'Debe ingresar al menos 3 caracteres';
       return false;
     }
 
-    if(this.lastParams === this.searchInput.value){
+    if (this.lastParams === this.searchInput.value) {
       return false;
     }
-    this.lastParams = this.searchInput.value
+    this.lastParams = this.searchInput.value;
 
-
-    if (this.searchInput.value.startsWith(' ') || this.searchInput.value.trim() === '') {
+    if (
+      this.searchInput.value.startsWith(' ') ||
+      this.searchInput.value.trim() === ''
+    ) {
       this.messageError = 'La búsqueda no puede empezar con espacios';
       return false;
     }
 
     return true;
-
   }
 
+  ordenar(check: HTMLInputElement) {
+    if (this.movies.length == 0) return;
+    if (check.checked) {
+      this.moviesKeep = JSON.parse(JSON.stringify(this.movies));
+      this.moviesSort = this.movies.sort((a, b) =>
+        a.title.localeCompare(b.title)
+      );
+      this.movies = this.moviesSort;
+    } else {
+      this.movies = this.moviesKeep;
+    }
+  }
 }
